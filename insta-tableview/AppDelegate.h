a8cbd69d92ffac4618f67622a8a14976db70ad23
Xuan Li <jamescowhen@gmail.com>
2012-07-11 10:05:59 -0700

    Initial Commit

---
+++ insta-tableview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  insta-tableview
+//
+//  Created by Xuan Li on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
